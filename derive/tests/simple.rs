b8f2b9a4e95b64e3700f675ab887d64bcc32a7aa
Ozaren <krishna.sd.2012@gmail.com>
2019-09-28 21:33:50 -0700

    updated test

--- derive/tests/simple.rs
+++ derive/tests/simple.rs
@@ -3,16 +3,25 @@
 use gfp_core::*;
 use gfp_derive::*;
 
-// #[derive(Field)]
-// struct Foo<T, U: Copy> {
-//     x: T,
-//     y: U
-// }
+#[derive(Field)]
+struct Foo<T, U: Copy> {
+    x: T,
+    y: U
+}
 
 #[derive(Field)]
 struct Bar<T>(u32, T);
 
 fn test() {
-    // let FooFields { x, y } = Foo::<u32, u16>::fields();
-    // let BarFields { } = Bar::fields();
+    let foo_fields = Foo::<Bar<f32>, i32>::fields();
+    let bar_fields = Bar::<f32>::fields();
+
+    let field = foo_fields.x.chain(bar_fields.1);
+
+    let foo = Foo {
+        x: Bar(0, 2.0),
+        y: 12
+    };
+
+    assert_eq!(*foo.project_to(field), 2.0);
 }
