b08c50816e9bfdf3c96c3bbb1f52150fe6f397e7
Ozaren <krishna.sd.2012@gmail.com>
2019-09-28 21:28:33 -0700

    added support for tuple structs

--- derive/tests/simple.rs
+++ derive/tests/simple.rs
@@ -4,11 +4,13 @@ use gfp_core::*;
 use gfp_derive::*;
 
 // #[derive(Field)]
-// #[repr(packed)]
 // struct Foo<T, U: Copy> {
 //     x: T,
 //     y: U
 // }
+
+#[derive(Field)]
+struct Bar<T>(u32, T);
 
 fn test() {
     // let FooFields { x, y } = Foo::<u32, u16>::fields();
