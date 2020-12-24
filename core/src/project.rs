98a9a49347779abc5742143181ddbacfca9c115d
RustyYato <krishna.sd.2012@gmail.com>
2020-12-24 15:03:32 -0500

    added tests for overlap fixed zst handling in overlap

--- core/src/project.rs
+++ core/src/project.rs
@@ -80,5 +80,52 @@ typsy::call! {
 }
 
 fn is_overlapping(a: Range<usize>, b: Range<usize>) -> bool {
-    a.contains(&b.start) || b.contains(&a.start)
+    !b.is_empty()
+        && !a.is_empty()
+        && (a.contains(&b.start) || b.contains(&a.start))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_overlapping;
+
+    // empty ranges can't overlap, because they represent zero sized types
+    // which can inhabit any address, and won't alias other values
+    #[test]
+    fn zst_no_overlap() {
+        assert!(!is_overlapping(1..1, 0..2));
+        assert!(!is_overlapping(1..1, 1..2));
+        assert!(!is_overlapping(1..1, 1..1));
+        assert!(!is_overlapping(1..1, 0..1));
+
+        assert!(!is_overlapping(0..2, 1..1));
+        assert!(!is_overlapping(1..2, 1..1));
+        assert!(!is_overlapping(1..1, 1..1));
+        assert!(!is_overlapping(0..1, 1..1));
+    }
+
+    #[test]
+    fn does_overlap() {
+        assert!(is_overlapping(1..4, 0..5));
+        assert!(is_overlapping(1..4, 0..2));
+        assert!(is_overlapping(1..4, 2..5));
+        assert!(is_overlapping(1..4, 2..3));
+
+        assert!(is_overlapping(0..5, 1..4));
+        assert!(is_overlapping(0..2, 1..4));
+        assert!(is_overlapping(2..5, 1..4));
+        assert!(is_overlapping(2..3, 1..4));
+
+        assert!(is_overlapping(1..4, 1..4));
+        assert!(is_overlapping(1..4, 0..4));
+        assert!(is_overlapping(0..4, 1..4));
+    }
+
+    #[test]
+    fn is_disjoint() {
+        assert!(!is_overlapping(1..4, 0..1));
+        assert!(!is_overlapping(1..4, 4..5));
+        assert!(!is_overlapping(1..4, 5..10));
+        assert!(!is_overlapping(5..10, 1..4));
+    }
 }
