109e78f16864b65a73cc63b562ccbfa94853717b
Cem Karan <ckaran@users.noreply.github.com>
2021-01-04 14:25:44 -0500

    Examples fix (#48)
    * feature: offset-based field (#35)
    
    * initial implementation of offset-based field
    * expanded on docs
    * replace `Field::Name` with `Field::range`
        * This allows `Dynamic` to be `Copy` and makes it easier to implement `Field`
    
    * fix: Moved modules into the 'src' directory.
    
    I made a mistake and put the modules in the root of the crate instead of
    the 'src' directory... 😅 Now fixed!
    
    * fix overlap detection
    
    previously there were cases where overlap checks didn't work
    in particular if one field completely covered another
    
    * Refactor: Extract from `TypeSet` to a new crate `typsy` (#36)
    
    * remove TypeSet
    * extract TypeSet's core utilities to a new crate
    * Add a link to #39 in  `Field::range` docs
    * added tests for overlap
        * fixed zst handling in overlap
    
    * Remove support for DST types and fields (#43)
    
    Fixes #39
    
    * remove support for DST types and fields
    * remove duplicate `Invariant` type
    remove public constructor for `Invariant` type, and replace with `const`
    this reduces the number of things the `derive` macro needs to know about
    * add a `pin_dynamic` method to `PinToPin`
    provides a safe interface to get a dynamic field.
    * Make `PinToPin::field` public (no reason to be private)
    * Fix `impl Field for Dynamic`
    
    * add unchecked projections trait
    
    * add tests for nonnull and option
    
    * removed `ProejctTo::Project: Deref`
    added `impl ProejctTo for Option`
    
    * add the `Identity` field
    
    * fix: Used an illegal name for a module.
    
    I used an illegal name for a module ('01_doubly_linked_list.rs') and
    didn't catch it because I forgot to update main to actually compile
    the code properly.  Both errors are now fixed.
    
    Co-authored-by: Yato <rustyyato@gmail.com>
    Co-authored-by: RustyYato <krishna.sd.2012@gmail.com>

--- core/src/project/from_arc.rs
+++ core/src/project/from_arc.rs
@@ -3,23 +3,26 @@
 //! This clones the `Arc` and keeps it around to clean up the data, and also
 //! holds on to a pointer to the field from the `Arc`'s allocation.
 
+use type_list::{FieldList, ProjectRaw, Projected};
+use typsy::map::{Map, Mapped};
+
 use super::*;
 
 use std::sync::Arc;
 
-pub struct ProjectedArc<P: ?Sized, T: ?Sized> {
+pub struct ProjectedArc<P, T> {
     _own:  Arc<P>,
     field: *const T,
 }
 
-unsafe impl<P: ?Sized, T: ?Sized> Send for ProjectedArc<P, T> where Arc<P>: Send
+unsafe impl<P, T> Send for ProjectedArc<P, T> where Arc<P>: Send
 {
 }
-unsafe impl<P: ?Sized, T: ?Sized> Sync for ProjectedArc<P, T> where Arc<P>: Sync
+unsafe impl<P, T> Sync for ProjectedArc<P, T> where Arc<P>: Sync
 {
 }
 
-impl<P: ?Sized, T: ?Sized> Deref for ProjectedArc<P, T> {
+impl<P, T> Deref for ProjectedArc<P, T> {
     type Target = T;
 
     fn deref(&self) -> &T {
@@ -27,7 +30,7 @@ impl<P: ?Sized, T: ?Sized> Deref for ProjectedArc<P, T> {
     }
 }
 
-unsafe impl<F: ?Sized> PinnablePointer for Arc<F> {
+unsafe impl<T: ?Sized> PinnablePointer for Arc<T> {
 }
 impl<F: Field> ProjectTo<F> for Arc<F::Parent> {
     type Projection = ProjectedArc<F::Parent, F::Type>;
@@ -43,53 +46,50 @@ impl<F: Field> ProjectTo<F> for Arc<F::Parent> {
     }
 }
 
-pub struct ProjectedArcSet<P: ?Sized, T: ?Sized> {
+pub struct ProjectedArcSet<P, T> {
     _own:  Arc<P>,
     field: T,
 }
 
-unsafe impl<P: ?Sized, T: ?Sized> Send for ProjectedArcSet<P, T> where
-    Arc<P>: Send
+unsafe impl<P, T> Send for ProjectedArcSet<P, T> where Arc<P>: Send
 {
 }
-unsafe impl<P: ?Sized, T: ?Sized> Sync for ProjectedArcSet<P, T> where
-    Arc<P>: Sync
+unsafe impl<P, T> Sync for ProjectedArcSet<P, T> where Arc<P>: Sync
 {
 }
 
-pub struct Split<P: ?Sized>(Arc<P>);
+pub struct Split<P>(Arc<P>);
 
-type_function! {
-    for(P: ?Sized, T: ?Sized)
-    fn(self: Split<P>, field: *const T) -> ProjectedArc<P, T> {
+typsy::call! {
+    fn[P, T](&mut self: Split<P>, field: *const T) -> ProjectedArc<P, T> {
         ProjectedArc { _own: self.0.clone(), field }
     }
 }
 
-impl<P: ?Sized, T> ProjectedArcSet<P, T> {
-    pub fn get<'a>(&'a self) -> TMap<T, PtrToRef<'a>>
+impl<P, T> ProjectedArcSet<P, T> {
+    pub fn get<'a>(&'a self) -> Mapped<T, PtrToRef<'a>>
     where
-        T: Copy + TupleMap<PtrToRef<'a>>,
+        T: Copy + Map<PtrToRef<'a>>,
     {
-        self.field.tup_map(PtrToRef(PhantomData))
+        self.field.map(PtrToRef(PhantomData))
     }
 
-    pub fn split(self) -> TMap<T, Split<P>>
+    pub fn split(self) -> Mapped<T, Split<P>>
     where
-        T: Copy + TupleMap<Split<P>>,
+        T: Copy + Map<Split<P>>,
     {
-        self.field.tup_map(Split(self._own))
+        self.field.map(Split(self._own))
     }
 }
 
-impl<'a, F: FieldSet> ProjectToSet<F> for Arc<F::Parent> {
-    type Projection = ProjectedArcSet<F::Parent, F::TypeSet>;
+impl<'a, Parent, F: FieldList<Parent>> ProjectAll<Parent, F> for Arc<Parent> {
+    type Projection = ProjectedArcSet<Parent, Projected<Parent, F>>;
 
     #[inline]
-    fn project_set_to(self, field: F) -> Self::Projection {
+    fn project_all(self, field: F) -> Self::Projection {
         unsafe {
             ProjectedArcSet {
-                field: field.project_raw(&self as &_),
+                field: field.map(ProjectRaw::new(&self as &_)),
                 _own:  self,
             }
         }
